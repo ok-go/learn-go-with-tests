2062dc86608382549fbfc48fc226218b07f303e6
Dmitriy Gordienko <d.gordienko@2gis.ru>
2020-04-26 22:13:37 +0700

    select

---
+++ select/racer_test.go
@@ -0,0 +1,45 @@
+package _select
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(10 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		want := fastServer.URL
+		got, err := Racer(fastServer.URL, slowServer.URL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10sec", func(t *testing.T) {
+		server := makeDelayedServer(20 * time.Millisecond)
+		defer server.Close()
+
+		if _, err := ConfigurableRacer(server.URL, server.URL, 10*time.Millisecond); err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
